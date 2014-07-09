57acf71e968b85a1f93481e2f6076849a31ee6bc
William Burgo <wburgo999@gmail.com>
2014-07-09 13:02:57 -0500

    problems 1-3

---
+++ Problem_1.js
@@ -0,0 +1,21 @@
+//Problem_1
+
+
+//Approach 1 - brute force
+var answer1a = 0;
+for(i = 0; i < 1000; i++) {
+	if(i % 3 == 0) 
+		answer1b += i;
+	else if (i % 5 == 0) 
+		answer1b += i;
+}
+
+//Approach 2 - using the the formula for arithmetic series
+function arithSeries(start, end, diff) {
+	return ((start + end)/2) * ((end - start)/diff + 1);
+}
+var answer1b = arithSeries(3,999,3) + arithSeries(5,995,5) - arithSeries(15,990,15);
+
+
+console.log(answer1a);
+console.log(answer1b);
