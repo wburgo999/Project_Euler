32f71c26ff44011e663c4f55024bbe4c925138ec
Will Burgo <will@cleermob.com>
2015-08-31 15:12:48 -0700

    Finish Problem 10

---
+++ Problem_10.js
@@ -0,0 +1,20 @@
+function sumPrimesToN( n ) {
+  var primes = [];
+  for ( var i = 0; i < n; i++ ) {
+    primes[i] = true;
+  }
+  primes[0] = primes[1] = false;
+  for ( var i = 2; i < n; i++ ) {
+    for ( var j = i * i; j < n; j += i) {
+      primes[j] = false;
+    }
+  }
+  var sum = 0;
+  primes.forEach( function( e, i ) {
+    if ( e ) sum += i;
+  } );
+  return sum;
+}
+
+console.log( sumPrimesToN( 2000000 ) )
+
