3a5c45927836cc25a7b876fee9043d889edd288f
Will Burgo <wburgo999@gmail.com>
2015-09-15 11:28:29 -0700

    Add Boilerplate BigInteger helper class

--- helpers/BigInteger.js
+++ helpers/BigInteger.js
@@ -1,18 +1,21 @@
 /* make big integer class */
 
 var BigInteger = function(num) {
-  this.num = String(num).split('').map(function(e) { return parseInt(e) });
+  this.digits = String(num).split('').map(function(e) { return parseInt(e) });
 }
 
 BigInteger.prototype.multiply = function(c) {
-  for (var i = 0; i < this.num.length; i++) {
-    var temp = c * this.num[i];
-    this.num[i] = temp % 10;
+  for (var i = 0; i < this.digits.length; i++) {
+    var temp = c * this.digits[i];
+    this.digits[i] = temp % 10;
     var remainder = Math.floor(temp / 10);
     if (i === 0) var first = remainder;
-    else this.num[i-1] += remainder; 
+    else this.digits[i-1] += remainder; 
   }
-  if (first) this.num.unshift(first);
+  if (first) this.digits.unshift(first);
 }
+
+BigInteger.prototype.add = function(num) {
+
 
 module.exports = BigInteger;
