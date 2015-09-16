c522e5963d077f763353a5c63c2281a4fd0dd74d
Will Burgo <wburgo999@gmail.com>
2015-09-16 10:58:51 -0700

    Add writeup and modify code - Problem 5

--- Problem_5/Problem_5.js
+++ Problem_5/Problem_5.js
@@ -1,7 +1,56 @@
-/*Problem 5
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
-*/
 
-//did prime factorizations by hand -- need to find a way to automate this for arbitrary n
-var result = 2*2*2*2*3*3*5*7*11*13*17*19;
+function smallestMultiple_1() {
+  var num = 21;
+  while (true) {
+    if (isDivisibleBy1To20(num)) return num;
+    num++;
+  }
+}
+
+function isDivisibleBy1To20(num) {
+  for (var i = 1; i <= 20; i++) {
+    if (num % i !== 0) return false;
+  }
+  return true;
+}
+
+function smallestMultiple_2(n) {
+  var lcmPrimeFactors = {};
+  var primeFactors;
+  for (var num = 1; num <= n; num++) {
+    primeFactors = getPrimeFactors(num);
+    combine(lcmPrimeFactors, primeFactors);
+  }
+  return multiplyFactors(lcmPrimeFactors);
+}
+function getPrimeFactors(num) {
+  var factors = {};
+  var factor = 2;
+  while (factor <= num) {
+    if (num % factor === 0) {
+      if (!factors[factor]) factors[factor] = 1;
+      else factors[factor]++;
+      num = num / factor;
+    }
+    else factor++
+  }
+  return factors;
+}
+function combine(factors, newFactors) {
+  for (var key in newFactors) {
+    if (!factors[key] || newFactors[key] > factors[key]) {
+      factors[key] = newFactors[key];
+    }
+  }
+  return factors;
+}
+function multiplyFactors(factors) {
+  var num = 1;
+  for (var key in factors) {
+    num *= Math.pow(key, factors[key]);
+  }
+  return num;
+}
+
+
+
